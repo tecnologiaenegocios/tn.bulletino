67c8ab991faa2abdca12c044925b73e2f5f534ce
Rafael Cabral Coutinho <rcabralc@tecnologiaenegocios.com.br>
2012-06-13 19:18:45 -0300

    Improve HTML mail output of styled pages

--- src/tn/bulletino/__init__.py
+++ src/tn/bulletino/__init__.py
@@ -2,10 +2,14 @@ from zope.i18nmessageid import MessageFactory
 _ = MessageFactory('tn.bulletino')
 
 from five import grok
+from plone.app.dexterity.behaviors.metadata import ICategorization
 from tn.plonehtmlimagecache.interfaces import IHTMLAttribute
 from tn.plonehtmlpage.html_page import IHTMLPageSchema
 from tn.plonemailing.interfaces import INewsletterHTML
 from tn.plonestyledpage import styled_page
+
+import zope.component
+import zope.globalrequest
 
 
 apply = lambda f: f()
@@ -37,27 +41,28 @@ class StyledPageNewsletterHTML(grok.Adapter):
     @property
     def html(self):
         return u"%(doctype)s\n<html %(html_attrs)s>%(head)s%(body)s</html>" % {
-            'doctype': u'',
-            'html_attrs': u'',
+            'doctype': u'<!DOCTYPE html>',
+            'html_attrs': u'lang="%s"' % self.get_language(),
             'head': self.get_head(),
-            'body': self.get_body(),
+            'body': u'<body>%s</body>' % self.context.body.output
         }
+
+    def get_language(self):
+        metadata = ICategorization(self.context, None)
+        if metadata and metadata.language:
+            return metadata.language
+        return self.get_language_from_portal()
+
+    def get_language_from_portal(self):
+        request = zope.globalrequest.getRequest()
+        portal_state = zope.component.getMultiAdapter(
+            (self.context, request),
+            name="plone_portal_state"
+        )
+        return portal_state.default_language()
 
     def get_head(self):
-        return u"<head>%(title)s%(style)s</head>" % {
-            'title': self.get_title(),
-            'style': self.get_style(),
+        return u"<head><title>%(title)s</title>%(style)s</head>" % {
+            'title': self.context.title,
+            'style': styled_page.getStyleBlock(self.context)
         }
-
-    def get_title(self):
-        return u"<title>%s</title>" % self.context.title
-
-    def get_style(self):
-        escaped_styles = styled_page.getEscapedStyles(self.context)
-        cdata_block = u"/*<![CDATA[*/%s/*]]>*/" % escaped_styles
-        return u'<style type="text/css" media="all">%s</style>' % cdata_block
-
-    def get_body(self):
-        return u'<body id="%s">%s</body>' % (
-            styled_page.getUniqueId(self.context), self.context.body.output
-        )
