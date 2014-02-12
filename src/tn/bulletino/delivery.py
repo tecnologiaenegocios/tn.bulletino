dba82878fbe3ccefd9cf0d3ec84686a94029e626
Rafael Cabral Coutinho <rcabralc@tecnologiaenegocios.com.br>
2014-02-12 16:39:01 -0200

    Warn the user if there's no subscriber to send to

--- src/tn/bulletino/delivery.py
+++ src/tn/bulletino/delivery.py
@@ -1,6 +1,9 @@
+# TODO Move this functionalities into tn.plonemailing.
+
 from five import grok
 from Products.statusmessages.interfaces import IStatusMessage
 from plone.directives import form
+from plone.memoize.instance import memoize
 from plone.supermodel import model
 from tn.bulletino import _
 from tn.plonemailing import interfaces as pminterfaces
@@ -55,6 +58,10 @@ class TestForm(form.SchemaForm):
     description = _(u'Send this newsletter to your own e-mail address in '
                     u'order to see how it will appear to your audience.')
 
+    def __init__(self, context, request, send_view=None):
+        super(TestForm, self).__init__(context, request)
+        self.send_view = send_view
+
     @button.buttonAndHandler(_(u'Perform test'))
     def handleTest(self, action):
         data, errors = self.extractData()
@@ -76,6 +83,10 @@ class TestForm(form.SchemaForm):
 
         IStatusMessage(self.request).add(_(u'Message successfully sent.'),
                                          type='info')
+
+        if self.send_view is not None:
+            self.send_view.notify_sent()
+
         self.redirect(self.context.absolute_url())
 
 
@@ -91,6 +102,10 @@ class SendForm(form.SchemaForm):
     description = _(u"By pressing the button below you will send this "
                     u'newsletter to all of your audience.')
 
+    def __init__(self, context, request, send_view=None):
+        super(SendForm, self).__init__(context, request)
+        self.send_view = send_view
+
     @button.buttonAndHandler(_(u'Send to all recipients'))
     def handleSend(self, action):
         data, errors = self.extractData()
@@ -103,6 +118,10 @@ class SendForm(form.SchemaForm):
 
         IStatusMessage(self.request).add(_(u'Message successfully sent.'),
                                          type='info')
+
+        if self.send_view is not None:
+            self.send_view.notify_sent()
+
         self.redirect(self.context.absolute_url())
 
 
@@ -110,15 +129,24 @@ class Send(grok.View):
     grok.require('tn.bulletino.SendContent')
     grok.context(behaviors.INewsletterFromContentMarker)
 
+    _sent = False
+
     def update(self):
         super(Send, self).update()
         self.test_form.update()
         self.send_form.update()
 
+        if len(self.subscribers()) == 0 and not self._sent:
+            IStatusMessage(self.request).add(
+                _(u'No subscribers were assigned to this newsletter yet. '
+                  u'Edit and add one or more subscriber lists.'),
+                type='warning'
+            )
+
     def __init__(self, context, request):
         super(Send, self).__init__(context, request)
-        self.test_form = TestForm(context, request)
-        self.send_form = SendForm(context, request)
+        self.test_form = TestForm(context, request, send_view=self)
+        self.send_form = SendForm(context, request, send_view=self)
 
     def subscriber_stats(self):
         formats = {}
@@ -128,6 +156,10 @@ class Send(grok.View):
             formats[subscriber.format] += 1
         return formats
 
+    @memoize
     def subscribers(self):
         behavior = behaviors.INewsletterFromContent(self.context)
-        return behavior.subscribers
+        return tuple(behavior.subscribers)
+
+    def notify_sent(self):
+        self._sent = True
